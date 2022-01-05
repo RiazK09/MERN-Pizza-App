1b53f9a21f82c01852ef15ce749c0526f2e1929a
Riaz Karolia <riazkarolia@gmail.com>
2022-01-05 14:29:09 +0200

    Commit

--- server.js
+++ server.js
@@ -49,20 +49,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(function (req, res, next) {
-  if (process.env.NODE_ENV === "production") {
-    const reqType = req.headers["x-forwarded-proto"];
-    // if not https redirect to https unless logging in using OAuth
-    if (reqType !== "https") {
-      req.url.indexOf("auth/google") !== -1
-        ? next()
-        : res.redirect("https://" + req.headers.host + req.url);
-    }
-  } else {
-    next();
-  }
-});
-
 // Deployment Code
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
